51d69c91d61fac5823837d0c0cfda2963b72fceb
Garrett Kadillak <gkadillak@gmail.com>
2017-09-04 22:20:02 -0700

    Update zenhub facade to request entire board at once

--- zef/facades/zenhub_facade.py
+++ zef/facades/zenhub_facade.py
@@ -4,12 +4,26 @@ import os
 import requests
 
 BASE_ENDPOINT = 'https://api.zenhub.io/p1'
+
 ISSUE_ENDPOINT = BASE_ENDPOINT + '/repositories/{repo_id}/issues/{issue_id}'
+BOARD_ENDPOINT = BASE_ENDPOINT + '/repositories/{repo_id}/board'
 
 API_TOKEN = os.environ.get('ZENHUB_API_TOKEN')
 
 logger = logging.getLogger(__file__)
 
+
+def _get_request(endpoint):
+    """
+    Make a GET request to the given endpoint
+
+    @param str endpoint: Who to request
+
+    @return Response
+    @rtype: requests.models.Response
+    """
+    headers = {'X-Authentication-Token': API_TOKEN}
+    return requests.get(endpoint, headers=headers)
 
 # token is sent in the X-Authentication-Token header
 def fetch_issue(repo_id=None, issue_id=None):
@@ -17,8 +31,24 @@ def fetch_issue(repo_id=None, issue_id=None):
     Fetch information about an issue from Zenhub
     """
     if not repo_id or not issue_id:
-        raise ValueError
+        raise ValueError('Repo ID and issue ID necessary for request')
 
     issue_endpoint = ISSUE_ENDPOINT.format(repo_id=repo_id, issue_id=issue_id)
-    headers = {'X-Authentication-Token': API_TOKEN}
-    return requests.get(issue_endpoint, headers=headers)
+    return _get_request(issue_endpoint)
+
+def fetch_board(repo_id):
+    """
+    Fetch the details of the board for a given sprint. This returns all
+    issues for all pipelines for the board excluding the closed issues.
+    For these issues, the github API is needed.
+
+    @param int repo_id: Number identifier of the repository (from github)
+
+    @return Board information
+    @rtype requests.models.Response
+    """
+    if not repo_id:
+        raise ValueError('Repo ID needed to make request')
+
+    board_endpoint = BOARD_ENDPOINT.format(repo_id=repo_id)
+    return _get_request(board_endpoint)
